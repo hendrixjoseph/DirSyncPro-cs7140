da0a314784cecc8fc4e1cdc8ae32c18ca7969265
Joseph Hendrix <hendrixjoseph@aol.com>
2016-04-11 21:01:15 -0400

    much cleanup

--- src/main/java/edu/wright/dirsyncpro/gui/jobdialog/BasicsTab.java
+++ src/main/java/edu/wright/dirsyncpro/gui/jobdialog/BasicsTab.java
@@ -33,23 +33,41 @@ import javax.swing.text.html.StyleSheet;
  */
 public final class BasicsTab extends JPanel {
 
+    private static final String destfieldTooltip = "<html>The destination directory. You can use the following wildcards:<br>\"<b>&lt;date</b>&gt;\" for the current date (or \"<b>&lt;DD</b>&gt;\" for day, \"<b>&lt;MM</b>&gt;\" for month, and \"<b>&lt;YYYY</b>&gt;\" for year) and<br>\"<b>&lt;time</b>&gt;\" for the current time (or \"<b>&lt;hh</b>&gt;\" for hour, \"<b>&lt;mm</b>&gt;\" for minute, and \"<b>&lt;ss</b>&gt;\" for second).</html>";
+    private static final String sourcefieldTooltip = "<html>The source directory. You can use the following wildcards:<br />\"<b>&lt;username&gt;</b>\" for the name of the current user, and <br />\"<b>&lt;userhome&gt;</b>\" for the home directory of the current user.</html>";
+
     private JComboBox<Const.SyncMode> syncModeComboBox = new JComboBox<>();
-    private JLabel dirDstLabel = new JLabel();
-    private JButton swapButton = new JButton();
-    private JLabel dirWithSubfoldersLabel = new JLabel();
     private JCheckBox dirWithSubfoldersCheckBox = new JCheckBox();
-    private JLabel dirSrcLabel = new JLabel();
-    private JLabel dirNameLabel = new JLabel();
     private JTextField dirNameField = new JTextField();
     private JTextField dirDstField = new JTextField();
-    private JButton dirDstChangeButton = new JButton();
-    private JButton dirSrcChangeButton = new JButton();
-    private JButton dirSrcChangeButton1 = new JButton();
     private JTextField dirSrcField = new JTextField();
     private JobDialogObjects jobDialog;
+    private JPanel pathsPanel = new JPanel();
 
     public BasicsTab(JobDialogObjects jobDialog) {
         this.jobDialog = jobDialog;
+        init();
+    }
+
+    private JPanel generateTitledPanel(String title) {
+        JPanel panel = new JPanel();
+
+        panel.setBorder(BorderFactory.createTitledBorder(title));
+        panel.setLayout(new GridBagLayout());
+
+        return panel;
+    }
+
+    private void init() {
+        JButton swapButton = new JButton();
+        JPanel syncModeJPanel = generateTitledPanel("Sync using this mode");
+        JPanel infoPanel = generateTitledPanel("Information");
+
+        JPanel jPanels[] = new JPanel[3];
+
+        for (int i = 0; i < jPanels.length; i++) {
+            jPanels[i] = new JPanel();
+        }
 
         setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
         setLayout(new GridBagLayout());
@@ -61,19 +79,8 @@ public final class BasicsTab extends JPanel {
         styles.addRule(bodyRule);
         editorKit.setStyleSheet(styles);
 
-        JPanel syncModeJPanel = new JPanel();
-        JPanel spacer = new JPanel();
-        JPanel pathsPanel = new JPanel();
-        JPanel infoPanel = new JPanel();
-        JLabel label = new JLabel();
         JTextPane jTextPane = new JTextPane();
         jTextPane.setEditorKit(editorKit);
-
-        JPanel jPanels[] = new JPanel[2];
-
-        for (int i = 0; i < jPanels.length; i++) {
-            jPanels[i] = new JPanel();
-        }
 
         GridBagConstraints gridBagConstraints = new GridBagConstraints();
         gridBagConstraints.gridx = 0;
@@ -85,109 +92,21 @@ public final class BasicsTab extends JPanel {
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 2;
         gridBagConstraints.weighty = 1.0;
-        add(spacer, gridBagConstraints);
+        add(jPanels[3], gridBagConstraints);
 
         pathsPanel.setBorder(BorderFactory.createTitledBorder("Label & Paths"));
         pathsPanel.setEnabled(false);
         pathsPanel.setLayout(new GridBagLayout());
 
-        dirSrcLabel.setIcon(new ImageIcon(getClass().getResource("/icons/dirGreen.png"))); // NOI18N
-        dirSrcLabel.setText("Dir A");
-        gridBagConstraints = new GridBagConstraints();
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 1;
-        gridBagConstraints.ipadx = 1;
-        gridBagConstraints.anchor = GridBagConstraints.EAST;
-        pathsPanel.add(dirSrcLabel, gridBagConstraints);
-
-        dirNameLabel.setIcon(new ImageIcon(getClass().getResource("/icons/name.png"))); // NOI18N
-        dirNameLabel.setText("Label");
-        dirNameLabel.setMaximumSize(null);
-        dirNameLabel.setMinimumSize(null);
-        dirNameLabel.setPreferredSize(null);
-        gridBagConstraints = new GridBagConstraints();
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 0;
-        gridBagConstraints.ipadx = 1;
-        gridBagConstraints.anchor = GridBagConstraints.EAST;
-        pathsPanel.add(dirNameLabel, gridBagConstraints);
-
-        dirNameField.setToolTipText("The label for this synchronization.");
-        dirNameField.setMaximumSize(null);
-        dirNameField.setMinimumSize(null);
-        dirNameField.setPreferredSize(null);
-        gridBagConstraints = new GridBagConstraints();
-        gridBagConstraints.gridx = 1;
-        gridBagConstraints.gridy = 0;
-        gridBagConstraints.gridwidth = 2;
-        gridBagConstraints.fill = GridBagConstraints.HORIZONTAL;
-        gridBagConstraints.anchor = GridBagConstraints.WEST;
-        gridBagConstraints.weightx = 0.01;
-        gridBagConstraints.insets = new Insets(4, 0, 4, 0);
-        pathsPanel.add(dirNameField, gridBagConstraints);
-
-        dirDstField.setToolTipText("<html>The destination directory. You can use the following wildcards:<br>\"<b>&lt;date</b>&gt;\" for the current date (or \"<b>&lt;DD</b>&gt;\" for day, \"<b>&lt;MM</b>&gt;\" for month, and \"<b>&lt;YYYY</b>&gt;\" for year) and<br>\"<b>&lt;time</b>&gt;\" for the current time (or \"<b>&lt;hh</b>&gt;\" for hour, \"<b>&lt;mm</b>&gt;\" for minute, and \"<b>&lt;ss</b>&gt;\" for second).</html>");
-        dirDstField.setInputVerifier(new PathVerifier(this.jobDialog, dirDstField));
-        dirDstField.setMaximumSize(null);
-        dirDstField.setMinimumSize(null);
-        dirDstField.setPreferredSize(null);
-        this.jobDialog.setFileDragNDrop(dirDstField);
-        gridBagConstraints = new GridBagConstraints();
-        gridBagConstraints.gridx = 1;
-        gridBagConstraints.gridy = 2;
-        gridBagConstraints.gridwidth = 2;
-        gridBagConstraints.fill = GridBagConstraints.HORIZONTAL;
-        gridBagConstraints.anchor = GridBagConstraints.WEST;
-        gridBagConstraints.weightx = 0.01;
-        pathsPanel.add(dirDstField, gridBagConstraints);
-
-        dirDstChangeButton.setFont(new Font("Tahoma", 0, 10)); // NOI18N
-        dirDstChangeButton.setIcon(new ImageIcon(getClass().getResource("/icons/browse.png"))); // NOI18N
-        dirDstChangeButton.setToolTipText("Browse for the destination directory.");
-        dirDstChangeButton.setIconTextGap(2);
-        dirDstChangeButton.setMargin(new Insets(2, 2, 2, 2));
-        dirDstChangeButton.addActionListener(this::dirDstChangeButtonbrowseDstActionPerformed);
-        gridBagConstraints = new GridBagConstraints();
-        gridBagConstraints.gridx = 3;
-        gridBagConstraints.gridy = 2;
-        gridBagConstraints.anchor = GridBagConstraints.EAST;
-        pathsPanel.add(dirDstChangeButton, gridBagConstraints);
-
-        dirSrcChangeButton.setFont(new Font("Tahoma", 0, 10)); // NOI18N
-        dirSrcChangeButton.setIcon(new ImageIcon(getClass().getResource("/icons/browse.png"))); // NOI18N
-        dirSrcChangeButton.setToolTipText("Browse for the source directory.");
-        dirSrcChangeButton.setIconTextGap(2);
-        dirSrcChangeButton.setMargin(new Insets(2, 2, 2, 2));
-        dirSrcChangeButton.addActionListener(this::dirSrcChangeButtonbrowseSrcActionPerformed);
-        gridBagConstraints = new GridBagConstraints();
-        gridBagConstraints.gridx = 3;
-        gridBagConstraints.gridy = 1;
-        gridBagConstraints.anchor = GridBagConstraints.EAST;
-        pathsPanel.add(dirSrcChangeButton, gridBagConstraints);
-
-        dirSrcField.setToolTipText("<html>The source directory. You can use the following wildcards:<br />\"<b>&lt;username&gt;</b>\" for the name of the current user, and <br />\"<b>&lt;userhome&gt;</b>\" for the home directory of the current user.</html>");
-        dirSrcField.setInputVerifier(new PathVerifier(this.jobDialog, dirSrcField));
-        dirSrcField.setMaximumSize(null);
-        dirSrcField.setMinimumSize(null);
-        dirSrcField.setPreferredSize(null);
-        this.jobDialog.setFileDragNDrop(dirSrcField);
-        gridBagConstraints = new GridBagConstraints();
-        gridBagConstraints.gridx = 1;
-        gridBagConstraints.gridy = 1;
-        gridBagConstraints.gridwidth = 2;
-        gridBagConstraints.fill = GridBagConstraints.HORIZONTAL;
-        gridBagConstraints.anchor = GridBagConstraints.WEST;
-        gridBagConstraints.weightx = 0.01;
-        pathsPanel.add(dirSrcField, gridBagConstraints);
-
-        dirDstLabel.setIcon(new ImageIcon(getClass().getResource("/icons/dirOrange.png"))); // NOI18N
-        dirDstLabel.setText("Dir B");
-        gridBagConstraints = new GridBagConstraints();
-        gridBagConstraints.gridx = 0;
-        gridBagConstraints.gridy = 2;
-        gridBagConstraints.ipadx = 1;
-        gridBagConstraints.anchor = GridBagConstraints.EAST;
-        pathsPanel.add(dirDstLabel, gridBagConstraints);
+        addLabel("/icons/dirGreen.png", "Dir A", 0, 1);
+        addLabel("/icons/name.png", "Label", 0, 0);
+        dirNameField = generateField("The label for this synchronization.", 1, 0);
+        dirDstField = generateField(destfieldTooltip, 1, 2);
+        addButton("/icons/browse.png", "Browse for the destination directory.", 2, 2, this::performDestinationDirectoryButtonAction);
+        addButton("/icons/browse.png", "Browse for the source directory.", 2, 1, this::performSourceDirectoryButtonAction);
+        addButton("/icons/GDrive.png", "Browse Google Drive", 3, 1, this::performGoogleDriveButtonAction);
+        dirSrcField = generateField(sourcefieldTooltip, 1, 1);
+        addLabel("/icons/dirOrange.png", "Dir B", 0, 2);
 
         jPanels[0].setMaximumSize(null);
         jPanels[0].setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
@@ -198,9 +117,8 @@ public final class BasicsTab extends JPanel {
         dirWithSubfoldersCheckBox.setPreferredSize(null);
         jPanels[0].add(dirWithSubfoldersCheckBox);
 
-        dirWithSubfoldersLabel.setIcon(new ImageIcon(getClass().getResource("/icons/withSubdirs.png"))); // NOI18N
-        dirWithSubfoldersLabel.setText("Include subfolders");
-        jPanels[0].add(dirWithSubfoldersLabel);
+
+        jPanels[0].add(generateLabel("/icons/withSubdirs.png","Include subfolders"));
 
         gridBagConstraints = new GridBagConstraints();
         gridBagConstraints.gridx = 2;
@@ -224,28 +142,7 @@ public final class BasicsTab extends JPanel {
         gridBagConstraints.gridy = 3;
         pathsPanel.add(swapButton, gridBagConstraints);
 
-        dirSrcChangeButton1.setFont(new Font("Tahoma", 0, 10)); // NOI18N
-        dirSrcChangeButton1.setIcon(new ImageIcon(getClass().getResource("/icons/GDrive.png")));
-        // NOI18N
-        dirSrcChangeButton1.setToolTipText("Browse for the source directory.");
-        dirSrcChangeButton1.setIconTextGap(2);
-        dirSrcChangeButton1.setMargin(new Insets(2, 2, 2, 2));
-        dirSrcChangeButton1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent evt) {
-//                 dirSrcChangeButton1browseSrcActionPerformed(evt);
-            }
-        });
-        gridBagConstraints = new GridBagConstraints();
-        gridBagConstraints.gridx = 3;
-        gridBagConstraints.gridy = 1;
-        gridBagConstraints.anchor = GridBagConstraints.EAST;
-        pathsPanel.add(dirSrcChangeButton1, gridBagConstraints);
-        dirSrcChangeButton1.getAccessibleContext().setAccessibleDescription("");
-
-        infoPanel.setBorder(BorderFactory.createTitledBorder("Information"));
         infoPanel.setPreferredSize(new Dimension(298, 180));
-        infoPanel.setLayout(new GridBagLayout());
 
         jTextPane.setBackground(UIManager.getDefaults().getColor("Label.background"));
         jTextPane.setContentType("text/html"); // NOI18N
@@ -265,16 +162,11 @@ public final class BasicsTab extends JPanel {
         gridBagConstraints.weightx = 1.0;
         add(infoPanel, gridBagConstraints);
 
-        syncModeJPanel.setBorder(BorderFactory.createTitledBorder("Sync using this mode"));
-        syncModeJPanel.setLayout(new GridBagLayout());
-
-        label.setIcon(new ImageIcon(getClass().getResource("/icons/DirSyncPro.png"))); // NOI18N
-        label.setText("Sync Mode:");
         gridBagConstraints = new GridBagConstraints();
         gridBagConstraints.fill = GridBagConstraints.HORIZONTAL;
         gridBagConstraints.anchor = GridBagConstraints.WEST;
         gridBagConstraints.insets = new Insets(0, 5, 0, 0);
-        syncModeJPanel.add(label, gridBagConstraints);
+        syncModeJPanel.add(generateLabel("/icons/DirSyncPro.png","Sync Mode:"), gridBagConstraints);
 
         syncModeComboBox.setMaximumRowCount(Const.SyncMode.values().length);
         syncModeComboBox.addItemListener(this::syncModeComboBoxItemStateChanged);
@@ -293,6 +185,63 @@ public final class BasicsTab extends JPanel {
         syncModeComboBox.setModel(new DefaultComboBoxModel<>(Const.SyncMode.values()));
         SyncModeComboboxCellRenderer smcbr = new SyncModeComboboxCellRenderer();
         syncModeComboBox.setRenderer(smcbr);
+    }
+
+    private JLabel generateLabel(String icon, String text) {
+        JLabel label = new JLabel();
+        label.setIcon(new ImageIcon(getClass().getResource(icon)));
+        label.setText(text);
+        return label;
+    }
+
+    private void addLabel(String icon, String text, int gridx, int gridy) {
+        GridBagConstraints gridBagConstraints = new GridBagConstraints();
+
+        gridBagConstraints.gridx = gridx;
+        gridBagConstraints.gridy = gridy;
+        gridBagConstraints.ipadx = 1;
+        gridBagConstraints.anchor = GridBagConstraints.EAST;
+
+        pathsPanel.add(generateLabel(icon,text), gridBagConstraints);
+    }
+
+    private void addButton(String icon, String tooltip, int gridx, int gridy, ActionListener listener) {
+        JButton button = new JButton();
+        GridBagConstraints gridBagConstraints = new GridBagConstraints();
+
+        button.setFont(new Font("Tahoma", 0, 10)); // NOI18N
+        button.setIcon(new ImageIcon(getClass().getResource(icon))); // NOI18N
+        button.setToolTipText(tooltip);
+        button.setIconTextGap(2);
+        button.setMargin(new Insets(2, 2, 2, 2));
+        button.addActionListener(listener);
+
+        gridBagConstraints.gridx = gridx;
+        gridBagConstraints.gridy = gridy;
+        gridBagConstraints.anchor = GridBagConstraints.EAST;
+        pathsPanel.add(button, gridBagConstraints);
+    }
+
+    private JTextField generateField(String tooltip, int gridx, int gridy) {
+        JTextField field = new JTextField();
+        GridBagConstraints gridBagConstraints = new GridBagConstraints();
+
+        field.setToolTipText(tooltip);
+        field.setInputVerifier(new PathVerifier(jobDialog, field));
+        field.setMaximumSize(null);
+        field.setMinimumSize(null);
+        field.setPreferredSize(null);
+        jobDialog.setFileDragNDrop(field);
+
+        gridBagConstraints.gridx = gridx;
+        gridBagConstraints.gridy = gridy;
+        gridBagConstraints.gridwidth = 1;
+        gridBagConstraints.fill = GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.anchor = GridBagConstraints.WEST;
+        gridBagConstraints.weightx = 0.01;
+        pathsPanel.add(field, gridBagConstraints);
+
+        return field;
     }
 
     public JComboBox<Const.SyncMode> getSyncModeComboBox() {
@@ -315,13 +264,17 @@ public final class BasicsTab extends JPanel {
         return dirWithSubfoldersCheckBox;
     }
 
-    private void dirDstChangeButtonbrowseDstActionPerformed(ActionEvent evt) {
-        GuiTools.browseFolder(this.jobDialog, dirDstField);
+    private void performDestinationDirectoryButtonAction(ActionEvent evt) {
+        GuiTools.browseFolder(jobDialog, dirDstField);
+    }
+
+    private void performGoogleDriveButtonAction(ActionEvent evt) {
+
     }
 
     private void syncModeComboBoxItemStateChanged(ItemEvent evt) {
         if (evt.getStateChange() == ItemEvent.SELECTED) {
-            this.jobDialog.syncModeComboBoxClicked();
+            jobDialog.syncModeComboBoxClicked();
         }
     }
 
@@ -331,7 +284,7 @@ public final class BasicsTab extends JPanel {
         dirDstField.setText(s);
     }
 
-    private void dirSrcChangeButtonbrowseSrcActionPerformed(ActionEvent evt) {
-        GuiTools.browseFolder(this.jobDialog, dirSrcField);
+    private void performSourceDirectoryButtonAction(ActionEvent evt) {
+        GuiTools.browseFolder(jobDialog, dirSrcField);
     }
 }
