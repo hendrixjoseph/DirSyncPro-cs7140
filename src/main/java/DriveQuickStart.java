889fbd7bc29d8313a3e363aafd984616d114a7df
Joseph Hendrix <hendrixjoseph@aol.com>
2016-04-13 21:02:43 -0400

    minor

--- src/main/java/DriveQuickStart.java
+++ src/main/java/DriveQuickStart.java
@@ -99,18 +99,20 @@ public class DriveQuickStart {
         // Build a new authorized API client service.
         Drive service = getDriveService();
 
+        
+
         // Print the names and IDs for up to 10 files.
         FileList result = service.files().list()
              .setPageSize(10)
              .setFields("nextPageToken, files(id, name)")
              .execute();
         List<File> files = result.getFiles();
-        if (files == null || files.size() == 0) {
+        if (files == null || files.isEmpty()) {
             System.out.println("No files found.");
         } else {
             System.out.println("Files:");
             for (File file : files) {
-                System.out.printf("%s (%s)\n", file.getName(), file.getId());
+                System.out.printf("%s (%s)\n", file.toString(), file.getId());
             }
         }
     }
